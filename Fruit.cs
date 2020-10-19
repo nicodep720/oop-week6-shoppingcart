491b16d67c383d807ba80483c7f627045515e371
Nicolas de POIX <22847@student.dorset-college.ie>
2020-10-19 15:23:09 +0100

    Ajoutez des fichiers projet.

---
+++ Fruit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppleOrange
+{
+    class Fruit
+    {
+        public double Price{get;set;}
+        public Fruit(double price)
+        {
+            price = this.Price;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AppleOrange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit apple = new Fruit(0.6);
+            Fruit orange = new Fruit(0.25);
+            apple.Price = 0.6;
+            orange.Price = 0.25;
+            Fruit[] basquet= { apple, apple, orange, apple };
+            double price = 0;
+            foreach (Fruit fruit in basquet) price += fruit.Price;
+            Console.WriteLine(price);
+            Console.ReadKey();
+        }
+    }
+}
